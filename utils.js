b804e2ba6ec397cea568e3f27ba119a630426e0f
Rick Beacham <rbeacham@eescoc.com>
2025-01-15 13:38:00 -0800

    Added Winston for better logging features.

--- utils.js
+++ utils.js
@@ -1,16 +1,30 @@
 // utils.js
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
 export function validateEnvVariables(requiredEnvVars) {
     requiredEnvVars.forEach(varName => {
         if (!process.env[varName]) {
-            throw new Error(`${varName} is not defined in the environment variables.`);
+            throw new Error(`Environment variable ${varName} is not defined. Please check your configuration.`);
         }
     });
 }
 
 export function logError(message, ...args) {
-    console.error(message, ...args);
+    logger.error(message, ...args);
 }
 
 export function logInfo(message, ...args) {
-    console.log(message, ...args);
+    logger.info(message, ...args);
 }
